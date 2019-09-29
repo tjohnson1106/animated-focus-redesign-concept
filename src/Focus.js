3151bf879e7f0b63f404716dc0bf2d5f7c04ec0b
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2019-09-28 22:40:42 -0400

    implemented transform values and conditions

--- src/Focus.js
+++ src/Focus.js
@@ -1,12 +1,55 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
-import Animated from "react-native-reanimated";
-import { TapGestureHandler, State } from "react-native-gesture-handler";
 
+import Animated, { Easing } from "react-native-reanimated";
+import { TapGestureHandler, State } from "react-native-gesture-handler";
 const { width, height } = Dimensions.get("window");
 
-const { Value, event, block, cond, eq, set } = Animated;
+const {
+  Value,
+  event,
+  block,
+  cond,
+  eq,
+  set,
+  Clock,
+  startClock,
+  stopClock,
+  debug,
+  timing,
+  clockRunning,
+  interpolate,
+  Extrapolate
+} = Animated;
 
+function runTiming(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0)
+  };
+
+  const config = {
+    duration: 1000,
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease)
+  };
+
+  return block([
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.position, value),
+      set(state.frameTime, 0),
+      set(config.toValue, dest),
+      startClock(clock)
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, debug("stop clock", stopClock(clock))),
+    state.position
+  ]);
+}
 class Focus extends Component {
   constructor() {
     super();
@@ -17,68 +60,272 @@ class Focus extends Component {
       {
         nativeEvent: ({ state }) =>
           block([
-            //   does state === STATE.END
-            //   state changes upon release of button for animation to run
-            cond(eq(state, State.END), set(this.buttonOpacity, 0))
+            cond(
+              eq(state, State.END),
+              set(this.buttonOpacity, runTiming(new Clock(), 1, 0))
+            )
           ])
       }
     ]);
+
+    this.buttonY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [100, 0],
+      extrapolate: Extrapolate.CLAMP
+    });
+
+    this.bgY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [-height / 3, 0],
+      extrapolate: Extrapolate.CLAMP
+    });
   }
   render() {
     return (
-      <View style={styles.root}>
-        <View style={{ ...StyleSheet.absoluteFill }}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "white",
+          justifyContent: "flex-end"
+        }}
+      >
+        <Animated.View
+          style={{
+            ...StyleSheet.absoluteFill,
+            transform: [{ translateY: this.bgY }]
+          }}
+        >
           <Image
-            source={require("../assets/bg_v2.jpg")}
-            style={{
-              flex: 1,
-              height: null,
-              width: null
-            }}
+            source={require("../assets/bg.jpg")}
+            style={{ flex: 1, height: null, width: null }}
           />
-        </View>
-        {/* email sign in/up */}
-        <View style={styles.buttonWrapper}>
+        </Animated.View>
+        <View style={{ height: height / 3, justifyContent: "center" }}>
           <TapGestureHandler onHandlerStateChange={this.onStateChange}>
-            <Animated.View style={{ ...styles.button, opacity: this.buttonOpacity }}>
-              <Text>SIGN IN</Text>
+            <Animated.View
+              style={{
+                ...styles.button,
+                opacity: this.buttonOpacity,
+                transform: [{ translateY: this.buttonY }]
+              }}
+            >
+              <Text style={{ fontSize: 20, fontWeight: "bold" }}>SIGN IN</Text>
             </Animated.View>
           </TapGestureHandler>
-          <View style={{ ...styles.button, backgroundColor: "#2E71DC" }}>
-            <Text>SIGN IN WITH FACEBOOK</Text>
-          </View>
+          <Animated.View
+            style={{
+              ...styles.button,
+              backgroundColor: "#2E71DC",
+              opacity: this.buttonOpacity,
+              transform: [{ translateY: this.buttonY }]
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "bold", color: "white" }}>
+              SIGN IN WITH FACEBOOK
+            </Text>
+          </Animated.View>
         </View>
-
-        {/* facebook sign in/up */}
       </View>
     );
   }
 }
+export default Focus;
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "flex-end"
-  },
-  buttonWrapper: {
-    height: height / 3,
-    justifyContent: "center"
-  },
   button: {
-    backgroundColor: "#fff",
+    backgroundColor: "white",
     height: 70,
     marginHorizontal: 20,
     borderRadius: 35,
     alignItems: "center",
     justifyContent: "center",
     marginVertical: 5
-  },
-  buttonText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#fff"
   }
 });
 
-export default Focus;
+// import React, { Component } from "react";
+// import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+// import Animated, { Easing } from "react-native-reanimated";
+// import { TapGestureHandler, State } from "react-native-gesture-handler";
+
+// const { width, height } = Dimensions.get("window");
+
+// const {
+//   Value,
+//   event,
+//   block,
+//   cond,
+//   eq,
+//   set,
+//   Clock,
+//   startClock,
+//   stopClock,
+//   debug,
+//   timing,
+//   clockRunning,
+//   interpolate,
+//   Extrapolate
+// } = Animated;
+
+// function runTiming(clock, value, dest) {
+//   const state = {
+//     finished: new Value(0),
+//     position: new Value(0),
+//     time: new Value(0),
+//     frameTime: new Value(0)
+//   };
+
+//   const config = {
+//     duration: 1000,
+//     toValue: new Value(0),
+//     easing: Easing.inOut(Easing.ease)
+//   };
+
+//   return block([
+//     cond(
+//       clockRunning(clock),
+//       [
+//         // if the clock is already running we update the toValue, in case a new dest has been passed in
+//         set(config.toValue, dest)
+//       ],
+//       [
+//         // if the clock isn't running we reset all the animation params and start the clock
+//         set(state.finished, 0),
+//         set(state.time, 0),
+//         set(state.position, value),
+//         set(state.frameTime, 0),
+//         set(config.toValue, dest),
+//         startClock(clock)
+//       ]
+//     ),
+//     // we run the step here that is going to update position
+//     timing(clock, state, config),
+//     // if the animation is over we stop the clock
+//     cond(state.finished, debug("stop clock", stopClock(clock))),
+//     // we made the block return the updated position
+//     state.position
+//   ]);
+// }
+
+// class Focus extends Component {
+//   constructor() {
+//     super();
+
+//     this.buttonOpacity = new Value(1);
+
+//     this.onStateChange = event([
+//       {
+//         nativeEvent: ({ state }) =>
+//           block([
+//             //   does state === STATE.END
+//             //   state changes upon release of button for animation to run
+//             cond(
+//               eq(state, State.END),
+//               set(
+//                 this.buttonOpacity,
+
+//                 runTiming(new Clock(), 1, 0)
+//               )
+//             )
+//           ])
+//       }
+//     ]);
+//     this.buttonY = interpolate(this.buttonOpacity, {
+//       inputRange: [0, 1],
+//       outputRange: [100, 0],
+//       extrapolate: Extrapolate.CLAMP
+//     });
+//     this.bgY = interpolate(this.buttonOpacity, {
+//       inputRange: [0, 1],
+//       outputRange: [-height / 3, 0],
+//       extrapolate: Extrapolate.CLAMP
+//     });
+//   }
+//   render() {
+//     return (
+//       <View style={styles.root}>
+//         <Animated.View
+//           style={{
+//             ...StyleSheet.absoluteFill,
+//             transform: [
+//               {
+//                 translateY: this.bgY
+//               }
+//             ]
+//           }}
+//         >
+//           <Image
+//             source={require("../assets/bg_v2.jpg")}
+//             style={{
+//               flex: 1,
+//               height: null,
+//               width: null
+//             }}
+//           />
+//         </Animated.View>
+//         {/* email sign in/up */}
+//         <View style={styles.buttonWrapper}>
+//           <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+//             <Animated.View
+//               style={{
+//                 ...styles.button,
+//                 opacity: this.buttonOpacity,
+//                 transform: [
+//                   {
+//                     translateY: this.buttonY
+//                   }
+//                 ]
+//               }}
+//             >
+//               <Text>SIGN IN</Text>
+//             </Animated.View>
+//           </TapGestureHandler>
+//           <View
+//             style={{
+//               ...styles.button,
+//               backgroundColor: "#2E71DC",
+//               opacity: this.buttonOpacity,
+//               transform: [
+//                 {
+//                   translateY: this.buttonY
+//                 }
+//               ]
+//             }}
+//           >
+//             <Text>SIGN IN WITH FACEBOOK</Text>
+//           </View>
+//         </View>
+
+//         {/* facebook sign in/up */}
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   root: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     justifyContent: "flex-end"
+//   },
+//   buttonWrapper: {
+//     height: height / 3,
+//     justifyContent: "center"
+//   },
+//   button: {
+//     backgroundColor: "#fff",
+//     height: 70,
+//     marginHorizontal: 20,
+//     borderRadius: 35,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginVertical: 5
+//   },
+//   buttonText: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//     color: "#fff"
+//   }
+// });
+
+// export default Focus;
